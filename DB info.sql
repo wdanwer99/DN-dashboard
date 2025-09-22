9992e910b8d573a7e88fee2ce6ca757cfbac0736
Mohamed-Mamoun <mamoun5612@gmail.com>
2025-09-22 05:22:56 -0700

    Comments added to DB file

--- DB info.sql
+++ DB info.sql
@@ -44,39 +44,41 @@ CREATE TABLE Delivery_Notes (
     print_date DATETIME,
     purpose_of_delivery TEXT,
     delivery_address TEXT,
-    site_Code VARCHAR(255),
-    contract_info VARCHAR(255),
-	Customer VARCHAR(255),
-	Customer_PO VARCHAR(255),
+    site_Code VARCHAR(255),-- gggg
+    contract_info VARCHAR(255), 00hjjbbn
+	Customer VARCHAR(255), jgjgjhghj
+	Customer_PO VARCHAR(255), gjgjhgj
 	Customer_Tel  VARCHAR(255),	
-	Project_Name VARCHAR(255),
+	Project_Name VARCHAR(255), ghjghjgj
 	Project_Code VARCHAR(255),
 	Product_Category VARCHAR(255),
-	Product_Manager VARCHAR(255),
+	Product_Manager VARCHAR(255), jhgjhghj
 	Special_Unloading_Req VARCHAR(255),
 	Installation_Environment VARCHAR(255),
 	Description_MR VARCHAR(255),
-	From_Warehouse VARCHAR(255),
-	Warehouse_Keeper VARCHAR(255),
+	From_Warehouse VARCHAR(255), jhgjhgjj
+	Warehouse_Keeper VARCHAR(255), ghfhfh
 	Warehouse_Keeper_tel VARCHAR(50),
 	Description_DN VARCHAR(255),
 	Including_dangerous_goods VARCHAR(255),
 	pickup_address TEXT,
 	Site_Address VARCHAR(255),
-	dn_no VARCHAR(50),
-    mr_no VARCHAR(50),
+	dn_no VARCHAR(50), gjhghjghjhj
+    mr_no VARCHAR(50), hffghfhhg
     receiver_name VARCHAR(255),
     receiver_tel VARCHAR(50),
 	Receiver_Company_Name VARCHAR(255),
-    request_arrived_date DATETIME,
+    request_arrived_date DATETIME, nggjhghj
     request_shipment_date DATETIME,
-    logistics_specialist VARCHAR(255),
+    logistics_specialist VARCHAR(255), ggjhghj
 	logistics_specialist_Tel VARCHAR(255),
-	received_location VARCHAR(255),
+	received_location VARCHAR(255), yytyut
 	received_Auto_location VARCHAR(255),
-	Collected_location VARCHAR(255),
+    Delivered_location VARCHAR(255), hjgjhgjhg
+	Delivered_Auto_location VARCHAR(255),
+	Collected_location VARCHAR(255), jyututu
 	Collected_Auto_location VARCHAR(255),
-	DN_Status ENUM('Created', 'Received', 'Delivered', 'Collected', 'Closed') DEFAULT 'Created',
+	DN_Status ENUM('Created', 'Received', 'Delivered', 'Collected', 'Closed') DEFAULT 'Created', hjghj
 	Company_code VARCHAR(50),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	Updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -90,7 +92,7 @@ CREATE TABLE Dn_items (
     item_code VARCHAR(50) NOT NULL,
     qty DECIMAL(12,2) NOT NULL,
     item_description TEXT,
-	Item_received_Image VARCHAR(255), -- ItemCode_Site_DNcode
+	Item_received_Image VARCHAR(255), -- image url name 
 	Item_Delivered_Image VARCHAR(255),
 	Item_Collected_Image VARCHAR(255),
 	Item_User_Created VARCHAR(255),
